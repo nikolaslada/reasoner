ed7caddade547eccdbdc515660c8df39f7af026f
Nikolas Lada <nikolas.lada@gmail.com>
2020-11-07 18:00:58 +0100

    Initial commit

---
+++ src/test/java/cz/nikolaslada/reasoner/ReasonerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.nikolaslada.reasoner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReasonerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
