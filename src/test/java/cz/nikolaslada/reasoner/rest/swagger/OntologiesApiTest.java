8d4a1605fe9a780087489df119bcb34f79ac0f1f
Nikolas Lada <nikolas.lada@gmail.com>
2020-11-30 23:00:24 +0100

    Unit test for OntologiesApi

---
+++ src/test/java/cz/nikolaslada/reasoner/rest/swagger/OntologiesApiTest.java
@@ -0,0 +1,41 @@
+package cz.nikolaslada.reasoner.rest.swagger;
+
+import cz.nikolaslada.reasoner.rest.swagger.domains.Ontologies;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Unit tests for ontologies")
+@ExtendWith(MockitoExtension.class)
+public class OntologiesApiTest {
+
+    @InjectMocks
+    private OntologiesApi ontologiesApi;
+
+    @DisplayName("Test get ontologies")
+    @Test
+    void getOntologiesTest() {
+        Ontologies response = ontologiesApi.getOntologies();
+
+        assertEquals(response.getCount(), 3);
+        assertEquals(response.getOffset(), 10);
+        assertEquals(response.getTotalCount(), 13);
+
+        assertEquals(response.getOntologyList().size(), 3);
+
+        assertEquals(response.getOntologyList().get(0).getId(), 1);
+        assertEquals(response.getOntologyList().get(0).getName(), "Test ontology #1");
+
+        assertEquals(response.getOntologyList().get(1).getClassCount(), 2147483647);
+        assertEquals(response.getOntologyList().get(1).getPropertyCount(), 32768);
+        assertEquals(response.getOntologyList().get(1).getIndividualCount(), 16777216);
+
+        assertEquals(response.getOntologyList().get(1).getOwner().getId(), 1);
+        assertEquals(response.getOntologyList().get(1).getOwner().getName(), "Admin");
+    }
+
+}
