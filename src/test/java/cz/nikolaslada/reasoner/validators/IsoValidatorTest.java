ce79b6147de721040b4200546538819d73202436
Nikolas Lada <nikolas.lada@gmail.com>
2021-02-25 21:54:58 +0100

    Checking Lang ISO List<Translation> in IsoValidator

--- src/test/java/cz/nikolaslada/reasoner/validators/IsoValidatorTest.java
+++ src/test/java/cz/nikolaslada/reasoner/validators/IsoValidatorTest.java
@@ -1,7 +1,12 @@
 package cz.nikolaslada.reasoner.validators;
 
+import cz.nikolaslada.reasoner.rest.swagger.domains.request.Translation;
+import cz.nikolaslada.reasoner.rest.swagger.exceptions.BadRequestException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,6 +27,29 @@ public class IsoValidatorTest {
         assertFalse(i.isIsoLanguageValid("cz"));
         assertFalse(i.isIsoLanguageValid("xx"));
         assertFalse(i.isIsoLanguageValid(""));
+    }
+
+    @DisplayName("Check List of Translation exception test")
+    @Test
+    void checkIsoListExceptionTest() {
+        String unknownIso = "XX";
+        List<Translation> translationList = Arrays.asList(
+                new Translation(
+                        unknownIso,
+                        "Test",
+                        "Test"
+                )
+        );
+
+        BadRequestException e = assertThrows(BadRequestException.class, () -> {
+            i.checkIsoList(translationList);
+        });
+
+        String expected = "Not supported Language ISO codes: ";
+        String actual = e.getMessage();
+
+        assertTrue(actual.contains(expected));
+        assertTrue(e.getErrorList().get(0).getData().get(0).contains(unknownIso));
     }
 
 }
