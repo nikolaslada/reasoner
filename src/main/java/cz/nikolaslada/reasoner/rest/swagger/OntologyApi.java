4d2833a728c7446c2beb1fcb14dc0cc9c8377848
Nikolas Lada <nikolas.lada@gmail.com>
2021-02-26 08:47:35 +0100

    Working POST /ontology ednpoint

--- src/main/java/cz/nikolaslada/reasoner/rest/swagger/OntologyApi.java
+++ src/main/java/cz/nikolaslada/reasoner/rest/swagger/OntologyApi.java
@@ -1,12 +1,20 @@
 package cz.nikolaslada.reasoner.rest.swagger;
 
+import cz.nikolaslada.reasoner.repository.model.Ontology;
 import cz.nikolaslada.reasoner.rest.swagger.domains.OntologyDetail;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import cz.nikolaslada.reasoner.rest.swagger.domains.request.NewOntology;
+import cz.nikolaslada.reasoner.rest.swagger.exceptions.ErrorException;
+import cz.nikolaslada.reasoner.services.OntologyService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class OntologyApi {
+
+    private final OntologyService service;
+
+    public OntologyApi(OntologyService service) {
+        this.service = service;
+    }
 
     @GetMapping("/ontology/{id}")
     public OntologyDetail getOntology(@PathVariable int id) {
@@ -17,6 +25,15 @@ public class OntologyApi {
                 32,
                 0
         );
+    }
+
+    @PostMapping(
+            value = "/ontology",
+            produces = { "application/json" },
+            consumes = { "application/json" }
+    )
+    public Ontology post(@RequestBody NewOntology request) throws ErrorException {
+        return this.service.save(request);
     }
 
 }
